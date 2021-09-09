acf8c784df8c29ad942ecca67220cec36f2cab3f
sophie <justanothersophie@gmail.com>
2021-09-09 15:54:18 -0400

    commit tree 213ae8bc4f0b519a555968329236ba1da401fc09

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,37 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name if self.artist
+  end
+
+  def genre_name
+    self.genre.name if self.genre
+  end
+
+  def note_contents=(note_contents_array)
+    note_contents_array.each do |note_contents|
+      if note_contents != ""
+        note = Note.create(content: note_contents)
+        self.notes << note
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.collect do |note|
+      note.content
+    end
+  end
 end
